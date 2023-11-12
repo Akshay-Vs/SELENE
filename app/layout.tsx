b50aea89af9187f829d0a9f96bf90dbdeb25a749
akshay-vs <akshay.vs2005@gmail.com>
2023-11-12 14:02:23 +0530

    FIX: Minor design tweaks

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1.25"
         />
-        <link rel="icon" href="/favicon.svg" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body className={inter.className}>{children}</body>
     </html>
